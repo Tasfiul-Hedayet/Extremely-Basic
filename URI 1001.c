6f6ecbb8533ad3efe63bb7f8ae1dd58d2784f667
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2020-06-29 21:26:21 +0600

    Update URI 1001.c

--- URI 1001.c
+++ URI 1001.c
@@ -2,8 +2,8 @@
 
 int main() {
 
-    int x, y;
-    scanf("%d%d", &x, &y);
-    printf("X = %d\n", x+y);
+    int x, y;//declare x, y variable
+    scanf("%d%d", &x, &y);//read two variable 
+    printf("X = %d\n", x+y);//add them together 
     return 0;
 }
